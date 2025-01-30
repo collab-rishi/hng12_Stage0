6240f5ae183d0ab119111de634e8661eff827a84
collab-rishi <convey.rishi@gmail.com>
2025-01-30 18:34:36 +0530

    Updated

--- index.js
+++ index.js
@@ -6,18 +6,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
 
     const email = "kumarrishi9862@gmail.com";
     let currentDateTime = new Date().toISOString();
     console.log(currentDateTime);
+    const githubUrl = "https://github.com/collab-rishi/hng12_Stage0"
 
     res.status(200).json({
         email: email,
         current_datetime: currentDateTime,
-
+        github_url: githubUrl
     });
 
+});
+app.listen(3000, () => {
+    console.log("Server is running on port 3000.")
 });
 
 module.exports=app;
